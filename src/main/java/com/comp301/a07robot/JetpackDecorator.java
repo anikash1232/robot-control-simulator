43dc17d5177f893c9bbf237e79a1f29fe1c0aeba
anikash1232 <anirudh.vpk@gmail.com>
2025-11-09 23:13:34 -0500

    dk whats going wrong

---
+++ src/main/java/com/comp301/a07robot/JetpackDecorator.java
@@ -0,0 +1,31 @@
+package com.comp301.a07robot;
+
+import javafx.scene.Parent;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.StackPane;
+
+public class JetpackDecorator extends RobotDecorator {
+
+    public JetpackDecorator(Robot decoratedRobot) {
+        super(decoratedRobot);
+    }
+
+    @Override
+    public String getDescription() {
+        return super.getDescription() + ", Jetpack";
+    }
+
+    @Override
+    public int getShield() {
+        return super.getShield() + 40;
+    }
+
+    @Override
+    public Parent getVisual() {
+        StackPane pane = (StackPane) decoratedRobot.getVisual();
+        ImageView jet = new ImageView(new Image("jetpack.png"));
+        pane.getChildren().add(jet);
+        return pane;
+    }
+}
